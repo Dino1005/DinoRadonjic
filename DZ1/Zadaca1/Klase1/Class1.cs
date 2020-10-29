a84958132e425a0b7bbd7212028fb0b8e13df405
Dino1005 <dino.radonjic@etfos.hr>
2020-10-29 22:02:07 +0100

    Initial commit. Added info.txt

---
+++ DZ1/Zadaca1/Klase1/Class1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Klase1
+{
+    public class Episode
+    {
+        private int vcount;
+        private double tscore;
+        private double mscore;
+
+        public int GetViewerCount() { return vcount; }
+        public double GetMaxScore() { return mscore; }
+
+        public Episode() { }
+
+        public Episode(int vcount, double tscore, double mscore)
+        {
+            this.vcount = vcount;
+            this.tscore = tscore;
+            this.mscore = mscore;
+        }
+
+        public double GetAverageScore() { return tscore/(double)vcount; }
+
+        public void AddView(double score)
+        {
+            vcount++;
+            tscore += score;
+            if (mscore < score)
+                mscore = score;
+        }
+
+
+    }
+}
